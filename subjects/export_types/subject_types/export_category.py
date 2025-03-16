b30591f99717023475dfcbad89efa307d0379723
animeshrick <animeshece198@gmail.com>
2025-03-17 00:06:53 +0530

    update export_category

--- subjects/export_types/subject_types/export_category.py
+++ subjects/export_types/subject_types/export_category.py
@@ -3,18 +3,11 @@ from typing import Optional
 from uuid import UUID
 
 from pydantic import BaseModel
-from subjects.models.category import Category
 
 
 class ExportCategory(BaseModel):
     id: Optional[UUID]
     name: str
-
-    def __init__(self, **kwargs):
-        category = kwargs.get("category")
-        if category and isinstance(category, Category):
-            kwargs["category"] = ExportCategory(**category.model_to_dict())
-        super().__init__(**kwargs)
 
 
 class ExportCategoryList(BaseModel):
